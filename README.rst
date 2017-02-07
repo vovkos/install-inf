796d481eab45b1384f76b983db9a807b407f8d44
Vladimir Gladkov <vovkos@gmail.com>
2017-02-07 16:17:28 +0800

    initial commit

---
+++ README.rst
@@ -0,0 +1,37 @@
+install-inf
+===========
+
+Abstract
+--------
+
+``install-inf.pl`` is a simple **Perl script** which can be used as a lighweight replacement for **Zadig** (http://zadig.akeo.ie)
+
+Motivation
+----------
+
+If you ever tried to replace a standard Windows driver for some USB device with a **libusb**-compatible one (such as the stock ``winusb.sys`` driver) then you know that it's not as simple as it sounds in the official Microsoft WinUSB guide.
+
+Going through the Device Manager and navigating to the ``.inf``-containing folder usually ends up with messages like *"The best driver for your device is already installed"* or *"The folder you specified doesn't contain a compatible software driver for your device"*.
+
+The trick is to *force-update* the driver. This can be achieved by calling ``UpdateDriverForPlugAndPlayDevices`` *SetupAPI* function while passing it the hardware ID of a device and the path to a particular ``.inf`` file.
+
+Usage
+-----
+
+1. Prepare the proper ``.inf`` file as described in the official Microsoft WinUSB guide at: https://msdn.microsoft.com/en-us/library/windows/hardware/ff540283(v=vs.85).aspx
+
+2. *[OPTIONAL]* Create a ``.cat`` file using ``inf2cat`` and sign it with ``signtool``
+
+3. Make sure **Perl** is accessiblve via ``PATH`` environment variable. Note that you need 64-bit Perl on 64-bit Windows.
+
+4. From an elevated command prompt run:
+
+	.. code:: bash
+
+		perl install-inf.pl <path-to-inf>
+
+5. Unless you have created a properly signed ``.cat`` file, you will see a *"Windows Security"* warning stating that *"Windows can't verify the publisher of this driver software"*. When you do, fearlessly hit *"Install this driver software anyway"*.
+
+6. Go to the Device Manager and make sure your device has moved from its original group into *"Universal Serial Bus devices"* and its name is now the same as in the ``.inf`` file.
+
+7. Congratulations! Now you can work with your device via **libusb** API.
---
+++ install-inf.pl
@@ -0,0 +1,100 @@
+use strict;
+use Win32::API;
+
+#...............................................................................
+
+my $infFileName = @ARGV [0] || die ("usage: install-inf.pl <inf-file>\n");
+
+open (my $f, '<', $infFileName) || die ("cannot open $infFileName: $!\n");
+
+my $hwId;
+while (my $s = <$f>)
+{
+	if ($s =~ m/([^\s]*VID_[0-9a-f]{4}&PID_[0-9a-f]{4}[^\s]*)/i)
+	{
+		$hwId = $1;
+		last;
+	}
+}
+
+if (!$hwId)
+{
+	die ("HWID not found in: $infFileName\n");
+}
+
+print ("installing $infFileName for HWID $hwId...\n");
+
+my $UpdateDriverForPlugAndPlayDevicesA = Win32::API->new (
+	"newdev.dll",
+	"BOOL
+	UpdateDriverForPlugAndPlayDevicesA (
+		HWND hwndParent,
+		LPCSTR HardwareId,
+		LPCSTR FullInfPath,
+		DWORD InstallFlags,
+		PBOOL bRebootRequired
+		)"
+	) || die ("cannot find newdev.dll:UpdateDriverForPlugAndPlayDevicesA: $!\n");
+
+my $NULL              = 0;
+my $INSTALLFLAG_FORCE = 1;
+
+my $result = $UpdateDriverForPlugAndPlayDevicesA->Call (
+	$NULL,
+	$hwId,
+	$infFileName,
+	$INSTALLFLAG_FORCE,
+	$NULL
+	);
+
+if (!$result)
+{
+	my $error = Win32::GetLastError ();
+	my $message = getWinErrorMessage ($error);
+	die (sprintf ("error (%d 0x%08x): %s\n", $error, $error, $message));
+}
+
+print ("done.\n");
+
+#...............................................................................
+
+sub getWinErrorMessage
+{
+	my ($error) = @_;
+
+	my $s = Win32::FormatMessage ($error);
+	if ($s)
+	{
+		return $s;
+	}
+
+	# try again with HRESULT_FROM_SETUPAPI
+
+	# special handling for setupapi errors is needed --
+	# FormatMessage fails unless setupapi errors are converted to HRESULT
+
+	my $FACILITY_WIN32         = 7;
+	my $FACILITY_SETUPAPI      = 15;
+	my $APPLICATION_ERROR_MASK = 0x20000000;
+	my $ERROR_SEVERITY_ERROR   = 0xC0000000;
+
+	my $mask = $APPLICATION_ERROR_MASK | $ERROR_SEVERITY_ERROR;
+	if (($error & $mask) == $mask)
+	{
+		$error = ($error & 0x0000ffff) | ($FACILITY_SETUPAPI << 16) | 0x80000000;
+	}
+	elsif (!($error & 0x80000000))
+	{
+		$error = ($error & 0x0000ffff) | ($FACILITY_WIN32 << 16) | 0x80000000;
+	}
+
+	my $s = Win32::FormatMessage ($error);
+	if ($s)
+	{
+		return $s;
+	}
+
+	return "<undefined-error>";
+}
+
+#...............................................................................
